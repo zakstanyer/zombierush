26368a488463fabecfddb35bfbcad12d7682adbd
zakstanyer <zakstanyer@icloud.com>
2025-06-23 15:16:18 +0100

    Added shooting bullets and rate of fire

--- Zombie Rush/Assets/Scripts/Gun.cs
+++ Zombie Rush/Assets/Scripts/Gun.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class Gun : MonoBehaviour
 {
@@ -14,29 +16,46 @@ public class Gun : MonoBehaviour
     };
 
     public GunType gunType;
+    [Header("Number of times the gun can shoot per second.")]
+    public float rateOfFire;
+    public GameObject bullet;
+
+    private float timer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime / rateOfFire;
+        }
     }
+
 
     public void Shoot()
     {
-        Ray ray = new Ray(spawn.position, spawn.forward);
-        RaycastHit hit;
-
-        float shotDistance = 20;
-        if (Physics.Raycast(ray, out hit, shotDistance))
+        if(timer <= 0)
         {
-            shotDistance = hit.distance;
+            timer = 1;
+            GameObject _bullet = Instantiate(bullet, spawn.transform.position, Quaternion.identity);
+            _bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 20, ForceMode.Impulse);
+
+            Ray ray = new Ray(spawn.position, spawn.forward);
+            RaycastHit hit;
+
+            float shotDistance = 20;
+            if (Physics.Raycast(ray, out hit, shotDistance))
+            {
+                shotDistance = hit.distance;
+            }
         }
-        Debug.DrawRay(ray.origin, ray.direction * shotDistance, Color.red, 1);
+
     }
 
     public void ShootContinuous()
